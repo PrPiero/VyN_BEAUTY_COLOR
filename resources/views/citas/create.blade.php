3ea899ea26ad9a696e2bdc5708f725775ec91c80
JoJose <the_unrequited@hotmail.com>
2021-06-15 12:24:50 -0500

    sexto commit

--- resources/views/citas/create.blade.php
+++ resources/views/citas/create.blade.php
@@ -3,40 +3,45 @@
 @section('contenido')
     <link rel="stylesheet" href="../css/app.css">
     <form action="{{route('citas.store')}}" method="POST">
+
         @csrf
         <div class="container">
             <div class="row">
                 <div class="pt-5">
                     <div class="col-sm p-4">
-                        <img src="../img/horarios.JPG" width="260" height="360">
+                        <img src="../img/horarios.JPG" width="360">
                     </div>
                 </div>
-                <div class="col-sm pt-5 p-4 mt-4">
+                <div style="max-width: 48%;"class="col-sm pt-5 p-4 mt-4">
                         <label class="pt-2">Para hacer una cita o reservación debes llenar todos los campos indicados a continuación para poder atenderte mejor.
                          Una vez realizada tu reserva nos estaremos comunicando contigo para confirmar la misma.</label>
                         <div class="row p-2">
 
-                            <div class="pl-2">
-                                Nombres y Apellidos* <input name="cliente" type="text" required>
+                            <div class="d-flex flex-column flex-nowrap">
+                                <div class="order-1 p-2">Nombres y Apellidos*</div>
+                                <input class="order-2 p-2" name="cliente" type="text" required>
                             </div>
-                            <div class="pl-2 pb-4">
-                            Telefono Contacto*<input name="telefono" type="number" maxlength="9" minlength="7" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" /><i>(Máximo 9 dígitos)</i>
+                            <div class="d-flex flex-column flex-nowrap">
+                                <div class="order-1 p-2">Telefono Contacto*</div>
+                                <input class="order-2 p-2" name="telefono" type="number" maxlength="9" minlength="7" oninput="if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" />
+                                <i class="order-3 p-2">(Máximo 9 dígitos)</i>
                             </div>
-                            <div class="pl-2 pb-2">
-                                Correo Electronico* <input name="email" type="email" required>
+                            <div class="d-flex flex-column flex-nowrap">
+                                <div class="order-1 p-2">Correo Electronico*</div>
+                                <input class="order-2 p-2" name="email" type="email" required>
                             </div>
                         </div>
-                        <div class="row p-2">
-                            <div class="p-2">
-                                Fecha* <input name="fecha" type="date" required>
+                        <div style="padding-bottom: .7rem;"class="row p-2">
+                            <div class="order-1 p-2">
+                                Fecha* <input style="border-radius: .2rem;margin-left: 1vh;padding-left: .3rem;"name="fecha" type="date" required>
                             </div>
                         </div>
                             <div>
-                            <div class="m-2">
-                                Escribe aquí tus requerimientos y/o el motivo de tu reserva*
+                            <div class="d-flex flex-column flex-nowrap">
+                                <div class="order-1 p-2">Escribe aquí tus requerimientos y/o el motivo de tu reserva*</div>
                             </div>
-                            <div class="m-2">
-                                <textarea name="descripcion" id="" cols="30" rows="10" required></textarea>
+                            <div style="padding: .2rem;"class="m-2">
+                                <textarea style="height: 60px;    margin-left: -.5rem;" class="order-2 p-2" name="descripcion" id="" cols="30" rows="10" required></textarea>
                             </div>
                             <div>
                               <button type="submit" class="btn btn-secondary">ENVIAR</button>
--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
     <!--<link href="{{-- asset('css/app.css') --}}" rel="stylesheet">-->
+    <link rel="stylesheet" href="/assets/stylesinicio/stylescitas.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Rochester&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&amp;family=Rochester&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/935a263ba2.js" crossorigin="anonymous"></script>
 
     <style>
         .msjenv{
@@ -25,7 +32,7 @@
             <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item" style='font-size: 24pt;'>
-            <a class="nav-link" href="/ReservarCita/Create">Reservar cita</a>
+            <a class="nav-link" href="/ReservarCita/Create">Servicios</a>
             </li>
             <li class="nav-item" style='font-size: 24pt;'>
             <a class="nav-link" href="#">Productos</a>
@@ -38,86 +45,18 @@
     </nav>
 
     @yield('contenido')
+<footer>
+        <div class="footerinicio">
+            <h1>VyN Beauty Color Salon</h1>
+            <h2> Nosotros | Horarios | Preguntas Frecuentes | Termino y condiciones | Politicas de Privacidad | Blog |
+                Libro de reclamaciones</h2>
+            <nav>
+                <a href=""><img src="/assets/img/vecwsp.png" alt=""></a>
+                <a href=""><img src="/assets/img/vecfb.png" alt=""></a>
 
-    <footer class="bg-dark text-center text-white">
-
-<section class="mb-4">
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-    repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
-    eum harum corrupti dicta, aliquam sequi voluptate quas.
-  </p>
-</section>
-<section class="">
-  <div class="row">
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-      <h5 class="text-uppercase">Links</h5>
-      <ul class="list-unstyled mb-0">
-        <li>
-          <a href="{{ route('index') }}" class="text-white">Inicio</a>
-        </li>
-        <li>
-          <a href="/ReservarCita" class="text-white">Reservar Cita</a>
-        </li>
-        <li>
-          <a href="#!" class="text-white">Productos</a>
-        </li>
-        <li>
-          <a href="#!" class="text-white">Acceder</a>
-        </li>
-      </ul>
-    </div>
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-      <h5 class="text-uppercase">Contactanos</h5>
-
-      <ul class="list-unstyled mb-0">
-        <li>
-        <a href="#!" class="text-white">Agenda tu cita</a>
-        </li>
-        <li>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-<path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-</svg><a href="#!" class="text-white">014688597</a>
-            </li>
-            <li>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-inbound" viewBox="0 0 16 16">
-<path d="M15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0zm-12.2 1.182a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-</svg> <a href="#!" class="text-white">969757529</a>
-            </li>
-            <li>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-plus" viewBox="0 0 16 16">
-<path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-<path fill-rule="evenodd" d="M12.5 1a.5.5 0 0 1 .5.5V3h1.5a.5.5 0 0 1 0 1H13v1.5a.5.5 0 0 1-1 0V4h-1.5a.5.5 0 0 1 0-1H12V1.5a.5.5 0 0 1 .5-.5z"/>
-</svg> <a href="#!" class="text-white">970532941</a>
-        </li>
-      </ul>
-    </div>
-    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-      <h5 class="text-uppercase">Links</h5>
-
-      <ul class="list-unstyled mb-0">
-        <li>
-          <a href="#!" class="text-white">Link 1</a>
-        </li>
-        <li>
-          <a href="#!" class="text-white">Link 2</a>
-        </li>
-        <li>
-          <a href="#!" class="text-white">Link 3</a>
-        </li>
-        <li>
-          <a href="#!" class="text-white">Link 4</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</section>
-</div>
-<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-© 2021 Copyright:
-<a class="text-white" href="https:VyNBeautyColor.com/">VyNBeautyColor.com</a>
-</div>
-<!-- Copyright -->
-</footer>
+            </nav>
+            <p>© 2021 Lima - Derechos reservados. Sitio por DevCop</p>
+        </div>
+    </footer>
 </body>
 </html>
